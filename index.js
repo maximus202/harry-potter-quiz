7d7d4bd9dd1f296daa18f0bb0bd1738452b5880d
Max R <maxx.ratto@gmail.com>
2019-11-02 15:21:08 -0700

    Added functionality to run getQuestion function when the start quiz button is clicked.

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ function startQuiz() {
     //needs to display a page within the .quiz-box that
     //welcomes the user and displays a button to start
     console.log('startQuiz function ran');
-    //display welcome image
-    $('.js-quiz-box').html('<img src="https://github.com/maximus202/harry-potter-quiz/blob/master/images/startpage.jpg?raw=true" alt="Image of harry potter glasses, wand, and scar."></br><button class=".js-start-quiz-button">Start Quiz</button>');
-    //display button
+    //display welcome image and start quiz button
+    $('.js-quiz-box').html('<img src="https://github.com/maximus202/harry-potter-quiz/blob/master/images/startpage.jpg?raw=true" alt="Image of harry potter glasses, wand, and scar."></br><button class="js-start-quiz-button">Start Quiz</button>');
+    //code below runsGetQuestion function when start quiz button is clicked.
+    $('.js-quiz-box').on('click', '.js-start-quiz-button', function() {
+        getQuestion();
+    });
 }
 
 function getQuestion() {
     //User stories: Present questions, select options from
     //multiple choices, submit answer.
-    console.log('displayQuestion function ran');
+    console.log('getQuestion function ran');
 }
 
 function getAccumulatedScore() {
