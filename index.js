f455f3170eba3e8091655086d0ffd17fc6db808c
Max R <maxx.ratto@gmail.com>
2019-11-02 13:42:42 -0700

    Added plain text file with user stories and js pseudocode.

---
+++ index.js
@@ -0,0 +1,35 @@
+function startQuiz() {
+    //User story: click a button to start the quiz
+    console.log('startQuiz function ran');
+}
+
+function getQuestion() {
+    //User stories: Present questions, select options from
+    //multiple choices, submit answer.
+    console.log('displayQuestion function ran');
+}
+
+function getAccumulatedScore() {
+    //User story: See accumulated score as user progresses
+    console.log('getAccumulatedScore function ran');
+}
+
+function getAnswer() {
+    //User story: Display whether they got the right or wrong answer
+    console.log('getAnswer function ran');
+}
+
+function moveToNextQuestion() {
+    //User story: Move to the next question after viewing the answer
+    console.log('moveToNextQuestion function ran');
+}
+
+function getResults() {
+    //User story: Get to the end of the quiz and see their overall score.
+    console.log('getResults function ran');
+}
+
+function retakeQuiz() {
+    //User story: retake the quiz
+    console.log('retakeQuiz function ran');
+}
