6ada3294081adcd0cd87b5ceb090b8d214a77e43
Max R <maxx.ratto@gmail.com>
2019-11-02 14:59:01 -0700

    Added start quiz button to start page but does not direct anywhere

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function startQuiz() {
     //needs to display a page within the .quiz-box that
     //welcomes the user and displays a button to start
     console.log('startQuiz function ran');
-    $('.js-quiz-box').html('<img src="https://github.com/maximus202/harry-potter-quiz/blob/master/images/startpage.jpg?raw=true" alt="Image of harry potter glasses, wand, and scar.">');
     //display welcome image
+    $('.js-quiz-box').html('<img src="https://github.com/maximus202/harry-potter-quiz/blob/master/images/startpage.jpg?raw=true" alt="Image of harry potter glasses, wand, and scar."></br><button>Start Quiz</button>');
     //display button
 }
 
