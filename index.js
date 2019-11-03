b75fbc688d85f8770d75340a716f9e40c630f967
Max R <maxx.ratto@gmail.com>
2019-11-02 20:54:24 -0700

    Added current variables for, img, imgAlt, question, choiceOne, choiceTwo, choiceThree, choiceFour. Also created the first question that shows up on the quiz.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 //Questions array below
 const STORE = [
-    {question: 'Question 1 of 5: In the Hall of Prophecy there are rows and rows of glowing orbs. Which row contains the prophecy about Harry and Voldemort?', img:'https://github.com/maximus202/harry-potter-quiz/blob/master/images/question1.jpg?raw=true',},
-    {question: 'Question 2 of 5: What is the address of Sirius Black’s family home?', img:'https://github.com/maximus202/harry-potter-quiz/blob/master/images/question2.jpg?raw=true'},
-    {question: 'Question 3 of 5: How many possible Quidditch fouls are there?', img:'https://github.com/maximus202/harry-potter-quiz/blob/master/images/question3.jpg?raw=true'},
-    {question: 'Question 4 of 5: What is Rita Skeeter’s animagus form?', img:'https://github.com/maximus202/harry-potter-quiz/blob/master/images/question4.jpg?raw=true'},
-    {question: 'Question 5 of 5: What condition gives Tonks the ability to transform her features?', img:'https://github.com/maximus202/harry-potter-quiz/blob/master/images/question5.jpg?raw=true'},
+    {for:'question1', question: 'Question 1 of 5: In the Hall of Prophecy there are rows and rows of glowing orbs. Which row contains the prophecy about Harry and Voldemort?', img:'https://github.com/maximus202/harry-potter-quiz/blob/master/images/question1.jpg?raw=true', imgAlt:'Image of Harry Potter from Harry Potter and the Order of the Phoenix.', choiceOne:'Row 79', choiceTwo:'Row 97', choiceThree:'Row 67', choiceFour:'Row 96'},
+    {for:'question2', question: 'Question 2 of 5: What is the address of Sirius Black’s family home?', img:'https://github.com/maximus202/harry-potter-quiz/blob/master/images/question2.jpg?raw=true'},
+    {for:'question3', question: 'Question 3 of 5: How many possible Quidditch fouls are there?', img:'https://github.com/maximus202/harry-potter-quiz/blob/master/images/question3.jpg?raw=true'},
+    {for:'question4', question: 'Question 4 of 5: What is Rita Skeeter’s animagus form?', img:'https://github.com/maximus202/harry-potter-quiz/blob/master/images/question4.jpg?raw=true'},
+    {for:'question5', question: 'Question 5 of 5: What condition gives Tonks the ability to transform her features?', img:'https://github.com/maximus202/harry-potter-quiz/blob/master/images/question5.jpg?raw=true'},
 ]
+console.log(STORE[2].question);
+console.log(STORE[2].img);
 
 function startQuiz() {
     //User story: click a button to start the quiz
@@ -14,15 +16,25 @@ function startQuiz() {
     console.log('startQuiz function ran');
     //display welcome image and start quiz button
     $('.js-quiz-box').html('<img src="https://github.com/maximus202/harry-potter-quiz/blob/master/images/startpage.jpg?raw=true" alt="Image of harry potter glasses, wand, and scar."></br><button class="js-start-quiz-button">Start Quiz</button>');
-    //code below runs getQuestion function when start quiz button is clicked.
+    //code below runs displayQuestion function when start quiz button is clicked.
     $('.js-quiz-box').on('click', '.js-start-quiz-button', function() {
-        getQuestion();
+        displayQuestion();
     });
 }
 
-function getQuestion() {
+function displayQuestion() {
     //User stories: Present questions, select options from
     //multiple choices, submit answer.
+    console.log('displayQuestion function ran')
+    let currentFor = STORE[0].for;
+    let currentImg = STORE[0].img;
+    let currentImgAlt = STORE[0].imgAlt
+    let currentQuestion = STORE[0].question;
+    let currentChoiceOne = STORE[0].choiceOne;
+    let currentChoiceTwo = STORE[0].choiceTwo;
+    let currentChoiceThree = STORE[0].choiceThree;
+    let currentChoiceFour = STORE[0].choiceFour;
+    $('.js-quiz-box').html('<form role="form" accept-charset="UTF-8" class="quiz-form"><fieldset><legend>Quiz</legend><img src="' + currentImg + '" alt="' + currentImgAlt + '"></br><label for="' + currentFor + '">' + currentQuestion + '</label></br><input type="radio" name="' + currentChoiceOne + '" value="' + currentChoiceOne + '">' + currentChoiceOne + '<br></fieldset></form>');
 }
 
 function getAccumulatedScore() {
